58f98a2c345b266f5df36e2a08d9741124703b52
Li Xianpeng <131100007@mail.smail.nju.edu.cn>
2017-10-25 20:17:45 +0800

    first commit

---
+++ build/CMakeFiles/typelist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/typelist.dir/main.cpp.o"
+  "typelist.pdb"
+  "typelist"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/typelist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
